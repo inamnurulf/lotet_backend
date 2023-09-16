dd776786995820ebd32efdcda5e56a5933988d59
inamnurul <aam.inam7310@gmail.com>
2023-09-16 15:37:25 +0700

    add: kerja praktik routing

---
+++ src/controllers/kerjaPraktik.js
@@ -0,0 +1,18 @@
+const { default: mongoose } = require('mongoose');
+const kerjaPraktikModels = require('../models/kerjaPraktikModels')
+
+exports.postKerjaPraktik = async (req, res, next) => {
+
+}
+
+exports.getKerjaPraktik = async (req, res, next) => {
+
+}
+
+exports.patchKerjaPraktik = async (req, res, next) => {
+    
+}
+
+exports.deleteKerjaPraktik = async (req, res, next) => {
+
+}

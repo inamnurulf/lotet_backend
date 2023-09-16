f1ca19ed74374612569a2e5c7c3d586f064c9f07
inamnurul <aam.inam7310@gmail.com>
2023-09-16 13:19:21 +0700

    fix: database initiation

--- index.js
+++ index.js
@@ -1,24 +1,16 @@
 require('dotenv').config()
+const initDB = require("./src/lib/db")
 
 const express = require('express')
 const app = express()
 const port = 3000
 
-const mongoose = require('mongoose')
-const options = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}
-mongoose.connect(process.env.DATABASE_URL, options)
-
-const db = mongoose.connection
-db.on('error', (error) => console.error(error))
-db.once('open', () => console.log('connected to db'))
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
+const db = initDB()
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-}) 
+  console.log(`Server is running on port : ${port}`)
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})

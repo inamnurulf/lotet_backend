4e4607eccb53b23de77e34b4919028795b0e9703
inamnurul <aam.inam7310@gmail.com>
2023-09-16 15:35:44 +0700

    add: basic routing and express parser

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 require('dotenv').config()
-const initDB = require("./src/lib/db")
+const initDB = require('./src/lib/db')
+
+const kerjaPraktikRoute =require('./src/routes/kerjaPraktik')
+const seminarRoute =require('./src/routes/seminar')
 
 const express = require('express')
 const app = express()
 const port = 3000
 
 const db = initDB()
+app.use(express.json())
 
 app.listen(port, () => {
   console.log(`Server is running on port : ${port}`)
 })
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use('/kerjaPraktik', kerjaPraktikRoute)
+app.use('/seminar', seminarRoute)
--- src/routes/seminar.js
+++ src/routes/seminar.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const Seminar = require('../models/seminar')
+const Seminar = require('../models/seminarModels')
 
 // code here
 

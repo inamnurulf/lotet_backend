178861b0c3fef555bf9834657cfb30b01387d3a6
BonarSs <bonaruli42@gmail.com>
2023-09-27 00:30:37 +0700

    bikin schema model  seminar

--- src/models/seminarModels.js
+++ src/models/seminarModels.js
@@ -3,7 +3,23 @@ const mongoose = require('mongoose')
 const seminarSchema = new mongoose.Schema({
     
     // input model here
+    name:{
+        type: String,
+        required: true,
+    },
+    details:{
+        type: String,
+        required: true,
+    },
+    image:{
+        type: String,
+        required: true,
+    },
+    category:[{
+        type: String,
+        required: false,
+    }]
 
-})
+}, {timestamps: true})
 
 module.exports = mongoose.model('Seminar', seminarSchema)

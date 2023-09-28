5961c8616e335c29d77e38160cdd6f8931652011
yasmine-az <yasmine.arfa.zahira>
2023-09-28 11:30:09 +0700

    Fix seminar controllers

--- src/controllers/seminar.js
+++ src/controllers/seminar.js
@@ -1,5 +1,5 @@
 const { default: mongoose } = require('mongoose');
-const kerjaPraktikModels = require('../models/kerjaPraktikModels')
+const seminarModels = require('../models/seminarModels')
 
 exports.postSeminar = async (req, res, next) => {
     try {

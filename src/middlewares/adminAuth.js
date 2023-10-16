af7ded252613b00687c5994d5fe8f277b5e9ec61
inamnurul <aam.inam7310@gmail.com>
2023-10-17 02:48:55 +0700

    feat: admin middleware

---
+++ src/middlewares/adminAuth.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+
+const requireAdmin = async (req, res, next) => {
+  const usertoken = await req.headers.cookie;
+  const Authorization = usertoken.split(" ")[0];
+  const token = Authorization.split("=")[1].replace(";", "");
+
+  if (token) {
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+      if (err) {
+        return res.status(401).json({ error: "Unauthorized" });
+      } else {
+        req.user = decoded;
+        if (req.user && req.user.payload.role === "admin") {
+            next();
+          } else {
+            return res.status(403).json({ error: "Forbidden" });
+          }
+      }
+    });
+  } else {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+};
+
+module.exports = requireAdmin;

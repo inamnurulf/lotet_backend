42fe51a026e21bf96749cc2cb86edfb848932d27
inamnurul <aam.inam7310@gmail.com>
2023-12-08 02:24:51 +0700

    fix: deployment v5

--- index.js
+++ index.js
@@ -1,33 +1,24 @@
 require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
 const initDB = require('./src/lib/db')
 
 const kerjaPraktikRoute =require('./src/routes/kerjaPraktik')
 const seminarRoute =require('./src/routes/seminar')
 const userRoute = require('./src/routes/user')
 const customRoute = require('./src/routes/custom')
-const bodyParser = require('body-parser')
-const express = require('express')
-const cors = require('cors')
 
 
 
 const app = express()
 app.use(cors());
+app.options('*', cors());
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 const port = 5000
 
 const db = initDB()
-
-app.use(cors())
-app.options('*', cors());
-app.use(express.limit(100000000));
-app.use(express.json())
-
-// app.use(bodyParser.json({ limit: '50mb' }));
-// app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
-
-
 // app.listen(port, () => {
 //   console.log(`Server is running on port : ${port}`)
 // })
